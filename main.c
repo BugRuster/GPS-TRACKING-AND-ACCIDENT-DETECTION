7c239bb41cf6393cfe5028ea6c3065736de27013
Anurag Sharma <90618167+proAnurag@users.noreply.github.com>
2023-01-21 19:20:06 +0530

    Update main.c
    Updated Class and added more functionality.

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #define CMD         0
 #define DATA        1
 
-#define Sensor      BIT0        // Channel A0
+#define Sensor      BIT0      
 #define AIN         BIT0
 
 #define LCD_OUT     P1OUT
@@ -22,13 +22,6 @@
 #define led2         BIT3
 #define led3         BIT4
 
-
-
-/**
- *@brief Delay function for producing delay in 0.1 ms increments
- *@param t milliseconds to be delayed
- *@return void
- **/
 void delay(uint16_t t)
 {
     uint16_t i;
@@ -36,10 +29,6 @@ void delay(uint16_t t)
         __delay_cycles(100);
 }
 
-/**
- *@brief Function to pulse EN pin after data is written
- *@return void
- **/
 void pulseEN(void)
 {
     LCD_OUT |= EN;
@@ -48,33 +37,23 @@ void pulseEN(void)
     delay(1);
 }
 
-/**
- *@brief Function to write data/command to LCD
- *@param value Value to be written to LED
- *@param mode Mode -> Command or Data
- *@return void
- **/
+
 void lcd_write(uint8_t value, uint8_t mode)
 {
     if(mode == CMD)
-        LCD_OUT &= ~RS;             // Set RS -> LOW for Command mode
+        LCD_OUT &= ~RS;  
     else
-        LCD_OUT |= RS;              // Set RS -> HIGH for Data mode
+        LCD_OUT |= RS;          
 
-    LCD_OUT = ((LCD_OUT & 0x0F) | (value & 0xF0));              // Write high nibble first
+    LCD_OUT = ((LCD_OUT & 0x0F) | (value & 0xF0));    
     pulseEN();
     delay(1);
 
-    LCD_OUT = ((LCD_OUT & 0x0F) | ((value << 4) & 0xF0));       // Write low nibble next
+    LCD_OUT = ((LCD_OUT & 0x0F) | ((value << 4) & 0xF0));       
     pulseEN();
     delay(1);
 }
 
-/**
- *@brief Function to print a string on LCD
- *@param *s pointer to the character to be written.
- *@return void
- **/
 void lcd_print(char *s)
 {
     while(*s)
@@ -84,12 +63,7 @@ void lcd_print(char *s)
     }
 }
 
-/**
- *@brief Function to move cursor to desired position on LCD
- *@param row Row Cursor of the LCD
- *@param col Column Cursor of the LCD
- *@return void
- **/
+
 void lcd_setCursor(uint8_t row, uint8_t col)
 {
     const uint8_t row_offsets[] = { 0x00, 0x40};
@@ -97,24 +71,20 @@ void lcd_setCursor(uint8_t row, uint8_t col)
     delay(1);
 }
 
-/**
- *@brief Function to change numeric value into it's corresponding char array
- *@param num Number which has to be displayed
- *@return void
- **/
+
 void lcd_printNumber(unsigned int num)
 {
-    char buf[3];                        // Creating a array of size 3
-    char *str = &buf[2];                // Initializing pointer to end of the array
+    char buf[3];                        
+    char *str = &buf[2];                
 
-    *str = '\0';                        // storing null pointer at end of string
+    *str = '\0';                      
 
     do
     {
-        unsigned long m = num;          // Storing number in variable m
-        num /= 10;                      // Dividing number by 10
-        char c = (m - 10 * num) + '0';  // Finding least place value and adding it to get character value of digit
-        *--str = c;                     // Decrementing pointer value and storing character at that character
+        unsigned long m = num;         
+        num /= 10;              
+        char c = (m - 10 * num) + '0';  
+        *--str = c;                    
     } while(num);
 
     lcd_print(str);
@@ -125,50 +95,38 @@ void lcd_init()
     LCD_DIR |= (D4+D5+D6+D7+RS+EN);
     LCD_OUT &= ~(D4+D5+D6+D7+RS+EN);
 
-    delay(150);                     // Wait for power up ( 15ms )
-    lcd_write(0x33, CMD);           // Initialization Sequence 1
-    delay(50);                      // Wait ( 4.1 ms )
-    lcd_write(0x32, CMD);           // Initialization Sequence 2
-    delay(1);                       // Wait ( 100 us )
-
-    // All subsequent commands take 40 us to execute, except clear & cursor return (1.64 ms)
-
-    lcd_write(0x28, CMD);           // 4 bit mode, 2 line
+    delay(150);                     
+    lcd_write(0x33, CMD);          
+    delay(50);                     
+    lcd_write(0x32, CMD);           
+    delay(1);                     
+    lcd_write(0x28, CMD);          
     delay(1);
 
-    lcd_write(0x0C, CMD);           // Display ON, Cursor OFF, Blink OFF
+    lcd_write(0x0C, CMD);          
     delay(1);
 
-    lcd_write(0x01, CMD);           // Clear screen
+    lcd_write(0x01, CMD);          
     delay(20);
 
-    lcd_write(0x06, CMD);           // Auto Increment Cursor
+    lcd_write(0x06, CMD);           
     delay(1);
 
-    lcd_setCursor(0,0);             // Goto Row 1 Column 1
+    lcd_setCursor(0,0);            
 }
-
-/**
- * @brief
- * These settings are wrt enabling ADC10 on Lunchbox
- **/
 
 void register_settings_for_ADC10()
 {
-    ADC10AE0 |= AIN;                            // P1.0 ADC option select
-    ADC10CTL1 = INCH_0;                         // ADC Channel -> 1 (P1.0)
-    ADC10CTL0 = SREF_0 + ADC10SHT_3 + ADC10ON;  // Ref -> Vcc, 64 CLK S&H , ADC - ON
+    ADC10AE0 |= AIN;                            
+    ADC10CTL1 = INCH_0;                        
+    ADC10CTL0 = SREF_0 + ADC10SHT_3 + ADC10ON;  
 }
 
-
-//@brief entry point for the code/
 void main(void) {
-    WDTCTL = WDTPW + WDTHOLD;                       //! Stop Watchdog (Not recommended for code in production and devices working in field)
-    //P1DIR &= ~BIT0;
+    WDTCTL = WDTPW + WDTHOLD;                    
 
-
-    lcd_init();                                     // Initializing LCD
-    lcd_write(0x01, CMD);                   // Clear screen
+    lcd_init();                                    
+    lcd_write(0x01, CMD);                  
     delay(20);
 
     register_settings_for_ADC10();
@@ -176,7 +134,7 @@ void main(void) {
 
     while(1)
     {
-        ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
+        ADC10CTL0 |= ENC + ADC10SC;    
 
         P2DIR &= ~Sensor;
 
@@ -185,14 +143,14 @@ void main(void) {
         P2DIR |= led1;
         P2DIR |= led2;
 
-        while(ADC10CTL1 & ADC10BUSY);           // Wait for conversion to end
+        while(ADC10CTL1 & ADC10BUSY);          
 
         float adc_value = 0;
 
-                adc_value = (ADC10MEM) * (99.99) / (1023.00);    // mapping 10-bit conversion result of ADC to corresponding voltage
+                adc_value = (ADC10MEM) * (99.99) / (1023.00);   
 
-                int int_part = adc_value;                                   // Integer part of calculated ADC value
-                int decimal_part = (adc_value - (float)int_part) * 10.0 ;  // Decimal part of calculated ADC value
+                int int_part = adc_value;                                  
+                int decimal_part = (adc_value - (float)int_part) * 10.0 ;  
 
 
 
@@ -202,10 +160,7 @@ void main(void) {
                     lcd_setCursor(1,9);
                                     lcd_print("Out");
                     lcd_setCursor(1,13);
-                                    lcd_printNumber(count);              // Displaying integer part of ADC value
-                                    //lcd_print(".");
-                                    //lcd_printNumber(decimal_part);          // Displaying decimal part of ADC value
-                                    delay(6000); }
+                                    lcd_printNumber(count);          
                else{
                    lcd_setCursor(0,0);
                                    lcd_print("");}
@@ -213,15 +168,13 @@ void main(void) {
                 lcd_setCursor(0,1);
                 lcd_print("Visitors");
                 lcd_setCursor(0,13);
-                lcd_printNumber(count);              // Displaying integer part of ADC value
-                //lcd_print(".");
-                //lcd_printNumber(decimal_part);          // Displaying decimal part of ADC value
+                lcd_printNumber(count);           
                 delay(6000);
 
 
 
 
-            if((P2IN & Sensor)) // If SW is Pressed
+            if((P2IN & Sensor)) 
                 {
                     lcd_write(0x01, CMD);
                     delay(20);
@@ -235,16 +188,6 @@ void main(void) {
 
                 }
 
-        //if(P1IN == 0x01) //  IF IR IS HIGH
-
-       // {
-
-           // lcd_setCursor(0,0);
-           // lcd_print("IR is high");
-
-       //delay(5000);//GIVE SOME DELAY
-
-       // }
 
         else
 
@@ -257,22 +200,7 @@ void main(void) {
             delay(5000);
 
         }
-       // float adc_value = 0;
-
-        //adc_value = (ADC10MEM) * (3.30) / (1023.00);    // mapping 10-bit conversion result of ADC to corresponding voltage
-
-        //int int_part = adc_value;                                   // Integer part of calculated ADC value
-        //int decimal_part = (adc_value - (float)int_part) * 10.0 ;  // Decimal part of calculated ADC value/
-
-
-
-
-        //lcd_setCursor(1,3);
-        //lcd_printNumber(int_part);              // Displaying integer part of ADC value
-        //lcd_print(".");
-        //lcd_printNumber(decimal_part);          // Displaying decimal part of ADC value
-        //lcd_setCursor(1,7);
-        //lcd_print("V");
+      
         delay(6000);
 
         if(count>0){
@@ -284,3 +212,4 @@ void main(void) {
 
     }
 }
+// code by -@bugruster
